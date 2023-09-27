7553809612f75b373bdcbfac9e86f7e57e7d6ab8
Henry Cope <henrydaviscope@gmail.com>
2023-09-27 16:03:03 -0600

    added renode to .ini

--- lib/lab/src/fifo.c
+++ lib/lab/src/fifo.c
@@ -9,10 +9,10 @@ void fifo_worker_handler(struct k_msgq *requests, struct k_msgq *results, int id
 
     while(1){
          
-        k_msgq_get(&requests, &cur_message, K_FOREVER);
+        k_msgq_get(requests, &cur_message, K_FOREVER);
         cur_message.output = cur_message.input + 5;
         cur_message.handled_by = id;
-        k_msgq_put(&results, &cur_message, K_FOREVER);
+        k_msgq_put(results, &cur_message, K_FOREVER);
 
     }
 }
