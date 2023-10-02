05addf9d39ffd65a9105f0afc72626864c5c41a0
Henry <henrydaviscope@gmail.com>
2023-10-02 13:35:01 -0600

    wrote signaling.c functions

--- lib/lab/src/signaling.c
+++ lib/lab/src/signaling.c
@@ -3,11 +3,19 @@
 #include "signaling.h"
 
 void signal_handle_calculation(struct k_sem *request, struct k_sem *response, struct signal_data *data){
+    k_sem_take(request, K_FOREVER);     //wait for request to be given
+    data->output = data->input +5;
+    k_sleep(K_MSEC(1));
+    k_sem_give(response);
+
 
 
 }
 
 int signal_request_calculate(struct k_sem *request, struct k_sem *response, struct signal_data *data){
+    k_sem_give(request);
+    int res = k_sem_take(response, K_MSEC(1000));
+    return res;
 
 
 }
